78bec71215f735ef44fd3c9d483022abfd757f91
max <nickitkacozloff@yandex.ru>
2025-01-26 06:44:00 +0300

    checking the randomizer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,9 @@ package org.example
 
 fun main() {
     println("Добро пожаловать в игру Виселица!")
+
+    val words = listOf("альтернатива", "виселица", "компьютер", "программа", "проект", "интерфейс", "класс")
+    val chosenWord = words.random()
+
+    println("Слово для игры: ${chosenWord}")
 }
