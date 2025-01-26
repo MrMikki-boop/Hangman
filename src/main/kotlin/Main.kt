33b99b1f4ee136b5e6699f879295e7f871c61471
max <nickitkacozloff@yandex.ru>
2025-01-26 06:48:11 +0300

    added word masking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,11 @@ fun main() {
     println("Добро пожаловать в игру Виселица!")
 
     val words = listOf("альтернатива", "виселица", "компьютер", "программа", "проект", "интерфейс", "класс")
-    val chosenWord = words.random()
+    val hiddenWord = hideWord(words.random())
 
-    println("Слово для игры: ${chosenWord}")
+    println("Слово для игры: $hiddenWord")
+}
+
+fun hideWord(word: String): String {
+    return word.replaceRange(0, word.length, "*".repeat(word.length))
 }
