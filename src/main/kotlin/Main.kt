a88ce1aa9190876fa3293def1c2dc41f71feed67
max <nickitkacozloff@yandex.ru>
2025-01-26 06:53:36 +0300

    added player input processing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,23 @@ fun main() {
     println("Добро пожаловать в игру Виселица!")
 
     val words = listOf("альтернатива", "виселица", "компьютер", "программа", "проект", "интерфейс", "класс")
-    val hiddenWord = hideWord(words.random())
+
+    val chosenWord = words.random()
+    val hiddenWord = hideWord(chosenWord)
 
     println("Слово для игры: $hiddenWord")
+
+    println("Введите букву:")
+    val input = readLine()?.lowercase()
+    println("Вы ввели: $input")
+
+    if (input != null && input in chosenWord) {
+        println("Есть такая буква!")
+    } else {
+        println("Нет такой буквы!")
+    }
+
+
 }
 
 fun hideWord(word: String): String {
